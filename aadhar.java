eb2a41d60683120e9c99b259ddd9b4e1510c6c88
11701057 <48952134+11701057@users.noreply.github.com>
2019-03-26 18:16:19 +0530

    Add files via upload

---
+++ aadhar.java
@@ -0,0 +1,23 @@
+class person
+{
+     class Aadhar 
+   {
+     void show()
+      {     
+     System.out.print("aadhar created");
+      }
+   }
+}
+
+class test
+{
+         public static void main(String...rk)
+      {   
+
+         person obj1=new person();
+         person.Aadhar obj2=obj1.new Aadhar();
+         obj2.show();
+      
+        }
+
+}
